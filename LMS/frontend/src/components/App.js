9dcc616bd478b62e21f7a4ea1706fd2153d7c42e
Ahmad Nakore <ahmadnakore@Ahmads-MacBook-Air.local>
2024-04-19 01:49:01 +0100

    fixed tailwind

---
+++ LMS/frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Frontend code goes here
+function App() {
+  return (
+    <div>
+      <h1 className="text-gray-800 font-bold text-5xl text-center drop-shadow-md">
+        Learning Management System
+      </h1>
+      <input type="text" placeholder="Search topic" className="input"></input>
+    </div>
+  );
+}
+
+export default App;
