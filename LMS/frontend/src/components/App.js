5977e56f6a918f3b264317e15e70b47c90561277
Auwalu Muhammad Izziddin <117743968+Auwaluncy@users.noreply.github.com>
2024-04-21 20:23:10 +0100

    navbar

--- LMS/frontend/src/components/App.js
+++ LMS/frontend/src/components/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h1 className="text-gray-800 font-bold text-5xl text-center drop-shadow-md">
-        Learning Management System
+        Learning Management System c
       </h1>
       <input type="text" placeholder="Search topic" className="input"></input>
     </div>
