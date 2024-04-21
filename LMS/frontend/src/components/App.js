935642d5b611843f720f3fff1c5c09ba965028f9
Auwalu Muhammad Izziddin <117743968+Auwaluncy@users.noreply.github.com>
2024-04-21 22:23:10 +0100

    done with the navigation bar

--- LMS/frontend/src/components/App.js
+++ LMS/frontend/src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Main from "./Nav/Main";
 
 // Frontend code goes here
 function App() {
   return (
     <div>
-      <h1 className="text-gray-800 font-bold text-5xl text-center drop-shadow-md">
-        Learning Management System c
+      {/* <h1 className="text-gray-800 font-bold text-5xl text-center drop-shadow-md">
+        Learning Management System 
       </h1>
       <input type="text" placeholder="Search topic" className="input"></input>
+  */ }
+  <Main />
     </div>
   );
 }
--- LMS/frontend/src/components/Nav/Main.js
+++ LMS/frontend/src/components/Nav/Main.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './Navbar'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import SignUp from './SignUp'
+import SignIn from './SignIn'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/signup' element={<SignUp />} />
+                <Route path='/signin' element={<SignIn />} /> 
+            </Routes>
+        </Router>
+    </div>
   )
 }
 
